226a4d90b33d848c7a58852fee82e035cd285cf8
gaoweicai <lili14520@gmail.com>
2020-06-10 21:10:27 +0800

    add eureka

---
+++ spring-cloud-eureka/src/main/java/com/code/genius/SpringCloudEurekaApplication.java
@@ -0,0 +1,17 @@
+package com.code.genius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author GaoWeicai.(lili14520 @ gmail.com)
+ * @date 2020/6/9
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringCloudEurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaApplication.class, args);
+    }
+}
